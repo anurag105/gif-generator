f51305a982d505c32ae4cb1be39d73ce78a82352
Anurag Thakur <22bme026@nith.ac.in>
2024-07-29 00:33:40 +0530

    Update useGif.js

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 // its recommended to keep the code in functions of react components as little as possilbe
 //since there was a lot of duplication in the code we used for gifs,
 // we can create a custom hook to handle the logic for fetching gifs
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+// const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
 const useGif = (tag) => {
@@ -21,7 +21,7 @@ const useGif = (tag) => {
 
     useEffect(() => {
         fetchData(tag);
-    }, []);
+    }, [tag]);
     //the dependency array includes [tag]. 
     //This ensures that the fetchData function is called whenever the tag value changes.
 
